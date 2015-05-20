95135aceab878fe1a85dbcf8fee8f2081042c18a
QiMENG <616684889@qq.com>
2015-05-20 22:26:19 +0800

    解析

--- Calligraphy/Service.m
+++ Calligraphy/Service.m
@@ -37,24 +37,105 @@ static NSString * const kBaseURLString = @"http://shufa.m.supfree.net/";
     return aStr;
 }
 
+/**
+ *  默认初始页数据
+ *
+ *  @param block aDataDic = @[@"text":@{}, @"author":@{}]
+ */
++ (NSURLSessionDataTask *) DefaultTextAndAuthor:(void (^)(NSDictionary *dic, NSError *error))block{
+    
+    return [[Service sharedClient] GET:@"index.asp"
+                            parameters:nil
+                               success:^(NSURLSessionDataTask *task, id responseObject) {
+                                   
+                                   block([self makeDictionaryFrom:responseObject], nil);
+                                   
+                               } failure:^(NSURLSessionDataTask *task, NSError *error) {
+                                   block(nil, error);
+                               }];
+    
+    return nil;
+}
++ (NSDictionary *)makeDictionaryFrom:(id)aData {
+    
+//    NSLog(@"%@",[self encodingGBKFromData:aData]);
+    
+    @autoreleasepool {
+        
+        GDataXMLDocument *doc = [[GDataXMLDocument alloc] initWithHTMLData:aData
+                                                                  encoding:CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingGB_18030_2000)
+                                                                     error:NULL];
+        if (doc) {
+            
+            NSArray *textList = [doc nodesForXPath:@"//div[@class='cdiv wenk']" error:NULL];
 
+            for (GDataXMLElement * elements in textList) {
+                
+//                NSLog(@"%@",elements.stringValue);
+                
+                NSLog(@"%@",[[elements attributeForName:@"title"] stringValue]);
+            }
+            
+            NSArray *authorList = [doc nodesForXPath:@"//div[@class='cdiv']" error:NULL];
+            
+            for (GDataXMLElement * elements in authorList) {
+                
+                NSLog(@"%@",elements.stringValue);
+                
+//                NSLog(@"%@",[[elements attributeForName:@"title"] stringValue]);
+            }
+        }
+    }
+    
+    
+    
+    return nil;
+}
+
+
+
+
+/**
+ *  获取page 文字
+ */
++ (NSURLSessionDataTask *) TextPage:(NSInteger)aPage
+                          withBlock:(void (^)(NSArray *array, NSError *error))block{
+    return nil;
+}
+
+/**
+ *  获取书法家列表
+ */
++ (NSURLSessionDataTask *) AllAuthor:(void (^)(NSArray *array, NSError *error))block{
+    return nil;
+}
+
+#pragma mark - 搜索书法家
++ (NSURLSessionDataTask *) SearchAuthor:(NSString *)aSearch
+                             parameters:(id)parameters
+                              withBlock:(void (^)(NSArray *posts, NSError *error))block{
+    return nil;
+}
+
+#pragma mark - 搜索文字
 + (NSURLSessionDataTask *) SearchText:(NSString *)aSearch
                     parameters:(id)parameters
                      withBlock:(void (^)(NSArray *posts, NSError *error))block {
-    
     
     return [[Service sharedClient] GET:aSearch
                             parameters:parameters
                                success:^(NSURLSessionDataTask *task, id responseObject) {
                                    
-                                   block([self listFromData:responseObject], nil);
+                                   block([self makeArrayFrom:responseObject], nil);
                                    
                                } failure:^(NSURLSessionDataTask *task, NSError *error) {
-                                   NSLog(@"%@",error);
+                                   block(nil, error);
                                }];
 }
-
-+ (NSArray *)listFromData:(id)aData {
+/**
+ *  生成数据列表 @{ DataItem }
+ */
++ (NSArray *)makeArrayFrom:(id)aData {
     
     NSMutableArray * mainArray = [NSMutableArray array];
     
--- Calligraphy/ViewController.m
+++ Calligraphy/ViewController.m
@@ -20,6 +20,14 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    //获取默认页
+    [Service DefaultTextAndAuthor:^(NSDictionary *dic, NSError *error) {
+        
+    }];
+    
+    
+    
+    //搜索
     NSString * str = @"raky.asp?zi=帝";
     
     [Service SearchText:[Service encodingBKStr:str] parameters:nil withBlock:^(NSArray *posts, NSError *error) {
